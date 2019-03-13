68211626069c51c6e6dbde333a8f424847182c76
hoant626 <hoant626@newschool.edu>
2019-03-13 13:37:42 -0400

    upload jquery excercise

--- projects/jquery-excercise/click-events/main.js
+++ projects/jquery-excercise/click-events/main.js
@@ -1,3 +1,10 @@
 $(function() {
-  // Your interactions go here
+  $( ".man" ).click(function() {
+  $( ".man" ).animate({
+    width: "+=200",
+    height: "+=200",
+  }, 5000, function() {
+  });
+});
+
 });
