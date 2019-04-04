48cd787e2db3fafc37b131583d06745b57fae872
hoant626 <hoant626@newschool.edu>
2019-04-03 21:11:00 -0400

    upload ama

---
+++ projects/ama/main.js
@@ -0,0 +1,50 @@
+$(function() {
+  var numbers = [];
+
+  var getRandomNumber = function() {
+    var number = Math.floor((Math.random() * 47) + 1);
+
+    while ( numbers.includes(number) ) {
+      number = Math.floor((Math.random() * 47) + 1);
+      console.log('trying to get another number');
+    }
+
+    console.log(numbers);
+
+    numbers.push(number);
+
+    return number;
+  }
+
+//popup
+  $('.close').click(function() {
+    $('.popup').css('visibility', 'hidden');
+    var number = getRandomNumber();
+    $('.card').eq(number).addClass('active');
+  })
+
+//thumbs
+  $('.thumbs').click(function() {
+    console.log("wow");
+
+    var activeGradient = $('.card.active img');
+
+    if ( $(this).hasClass('up') ) {
+      $( '.yes' ).append( activeGradient.clone().addClass('active') );
+    } else {
+      $( '.no' ).append( activeGradient.clone().addClass('active') );
+    }
+
+    $('.card').removeClass('active');
+    var number = getRandomNumber();
+    $('.card').eq(number).addClass('active');
+  })
+
+//up and down
+  // $('.up').click(function() {
+  //   var number = Math.floor((Math.random() * 49) + 1);
+  //   var activeGradient = $('.card.active img');
+  //   $( '.yes' ).append( activeGradient.clone() );
+  // })
+
+});
