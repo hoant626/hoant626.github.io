3a6f0176342aec2222d481d46781132004da85e1
hoant626 <hoant626@newschool.edu>
2019-04-19 10:56:38 -0400

    update evolving website

--- projects/evolving-website/sketch.js
+++ projects/evolving-website/sketch.js
@@ -1,4 +1,16 @@
 var animal1;
+
+
+$(function() {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://www.movebank.org/movebank/service/public/json?study_id=2911040&individual_local_identifiers[]=4262-84830876&sensor_type=gps"; // site that doesn’t send Access-Control-*
+fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+.then(response => response.text())
+.then(contents => console.log(contents))
+.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+)};
+
 function setup() {
   // put setup code here
   createCanvas(1000,1000);
